eec12f55891bce062536c4dc46d90bfacfcfcaa4
Thomas Martinez <tmartinez114@gmail.com>
2021-09-20 22:52:39 -0400

    added pokemon stats to page and styled

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -50,7 +50,15 @@ const App = () => {
         {!pokemonChosen ? (
           <h1>Please Choose a Pokemon</h1>
         ) : (
-          <h1>{pokemonName}</h1>
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img} />
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type: {pokemon.type}</h3>
+            <h4>HP: {pokemon.hp}</h4>
+            <h4>Attack: {pokemon.attack}</h4>
+            <h4>Defense: {pokemon.defense}</h4>
+          </>
         )}
       </div>
     </div>
