2e9faa42b20216b11d9775f12780bf1febaaa473
Thomas Martinez <tmartinez114@gmail.com>
2021-09-20 23:17:56 -0400

    added border to pokemon card

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -51,13 +51,15 @@ const App = () => {
           <h1>Please Choose a Pokemon</h1>
         ) : (
           <>
-            <h1>{pokemon.name}</h1>
-            <img src={pokemon.img} />
-            <h3>Species: {pokemon.species}</h3>
-            <h3>Type: {pokemon.type}</h3>
-            <h4>HP: {pokemon.hp}</h4>
-            <h4>Attack: {pokemon.attack}</h4>
-            <h4>Defense: {pokemon.defense}</h4>
+            <div className='card'>
+              <h1>{pokemon.name}</h1>
+              <img src={pokemon.img} alt={pokemon.name} />
+              <h3>Species: {pokemon.species}</h3>
+              <h3>Type: {pokemon.type}</h3>
+              <h4>HP: {pokemon.hp}</h4>
+              <h4>Attack: {pokemon.attack}</h4>
+              <h4>Defense: {pokemon.defense}</h4>
+            </div>
           </>
         )}
       </div>
